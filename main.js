946fd15d5214976b448a24b7a9cbfc43b5999cf6
junyoung-moon <1053759@naver.com>
2020-09-27 20:48:27 +0900

    수업따라가기

--- main.js
+++ main.js
@@ -2,24 +2,40 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
+var app = http.createServer((request,response) => {
     var URL = request.url;
     var queryData = url.parse(URL, true).query;
-    console.log(queryData);
-    if(request.url == '/'){
-      URL = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    console.log(__dirname+URL);
-    response.end(fs.readFileSync(__dirname + URL));
- 
+    console.log(queryData.id);
+    var Title = queryData.id;
+    fs.readFile(`data/${Title}`, 'utf-8', (err, data) => {
+      var Template = 
+        `<!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${Title}</title>
+            </head>
+            <body>
+              <h1><a href="index.html">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${Title}</h2>
+              <p style="margin-top:45px;">
+              ${data}
+              </p>
+            </body>
+          </html>
+        `;
+      response.writeHead(200);
+      response.end(Template);
+    });
 });
-app.listen(8050);
+var port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+  console.log(`Server Starting at http://localhost:${port}`);
+});
 
 
 // URL === http://opentutorials.org:3000/main?id=HTML&page=12
